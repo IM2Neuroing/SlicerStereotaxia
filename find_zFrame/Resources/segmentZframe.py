450ae010eaea013784b73f9c30375d639950bede
Dorian Vogel <dorianvogel@gmail.com>
2024-05-03 07:15:19 +0200

    Big reorganization. Both CT and MR now work, but with different approaches for the segmentation. The CT still uses threshold, but the MR uses edge detection. Downstream shape analysis is common.

--- find_zFrame/Resources/segmentZframe.py
+++ find_zFrame/Resources/segmentZframe.py
@@ -11,7 +11,135 @@ def segment_zFrame(in_img: sitk.Image, img_type="MR", withPlots=False):
     except ImportError:
         print("WARNING: Failed to import matplotlib, not plots will be produced !")
         withPlots = False
-    import math
+
+    def isPlate_size(bbox):
+        obb_size = np.array(bbox)
+        if np.product(obb_size) == 0:
+            return False
+        # compute the ratio between the smallest and the largest side of obb
+        obb_ratio = max(obb_size) / min(obb_size)
+        image_occupy = np.sum(obb_size >= 0.5 * min(in_img_np.shape))
+        return (image_occupy >= 1) and (obb_ratio > 4) and (np.sum(obb_size > 100) >= 2)
+
+    def isPlate_loc(centroid, allObjects_bboxCenter, allObjects_bboxSize):
+        return (
+            np.linalg.norm(np.array(centroid) - np.array(allObjects_bboxCenter))
+            > np.min(allObjects_bboxSize) * 0.3
+        )
+
+    def threshold_plates_CT(in_img, minThreshold_byCount, hist_y, hist_x, withPlots=False):
+        b, a = butter(2, 0.15, btype="low", analog=False)
+        hist_y = filtfilt(b, a, hist_y)
+
+        hist_diff = np.diff(hist_y)
+
+        hist_diff_zc = np.where(np.diff(np.sign(hist_diff)) == 2)[0].flatten()
+        if withPlots:
+            print(hist_x[hist_diff_zc])
+
+        # minThreshold_byVariation = hist_x[hist_diff_zc[hist_x[hist_diff_zc]>secondSpikeVal_intens]][0]
+        minThreshold_byVariation_id = hist_diff_zc[
+            hist_x[hist_diff_zc] > (minThreshold_byCount + 100)
+        ][0]
+        minThreshold_byVariation = hist_x[minThreshold_byVariation_id]
+        print("first maxima after soft tissue found: %f" % minThreshold_byVariation)
+
+        if withPlots:
+            fig, ax = plt.subplots(2, 1, sharex=True)
+            ax[0].plot(hist_x, hist_y, label="intensity histogram")
+            ax[1].plot(hist_x, cumHist_y, label="cumulative histogram")
+            ax[1].axvline(x=minThreshold_byCount, ymin=0.0, ymax=1.0, color="r")
+
+            ax[0].plot(hist_x[1:], hist_diff, label="derivative of intensity histogram")
+            ax[0].plot(
+                hist_x[hist_diff_zc],
+                hist_diff[hist_diff_zc],
+                linestyle="",
+                marker="o",
+                label="zero crossings of derivative of intensity histogram",
+            )
+
+            ax[0].axvline(x=minThreshold_byCount, ymin=0.0, ymax=1.0, color="r")
+            ax[0].axvline(x=minThreshold_byVariation, ymin=0.0, ymax=1.0, color="g")
+            fig.legend()
+            ax[0].grid()
+
+        middleSlice = np.array(np.floor(np.array(in_img_np.shape) / 2), dtype=int)
+
+        thresh_img = in_img > minThreshold_byVariation
+
+        # autocrop the thresholded volume to remove any background... (matlab...)
+        # so that we can use relative sizes in `labelToKeep_mask`
+        stats = sitk.LabelShapeStatisticsImageFilter()
+        stats.Execute(thresh_img)
+        thresh_img = sitk.Crop(thresh_img, stats.GetBoundingBox(1))
+
+        if withPlots:
+            plt.figure()
+            plt.imshow(sitk.GetArrayFromImage(thresh_img)[:, :, middleSlice[2]])
+
+        connectedComponentImage = sitk.ConnectedComponent(thresh_img)
+        return connectedComponentImage
+
+    def threshold_plates_MR(in_img, minThreshold_byCount, withPlots=False):
+        p90_img = in_img * sitk.Cast(in_img > minThreshold_byCount, in_img.GetPixelIDValue())
+        # Mask in_image with minthreshold_byCount
+        fg_img = p90_img * sitk.Cast(p90_img > minThreshold_byCount, p90_img.GetPixelIDValue())
+        # do edge detection on the fg_img
+        edge_img = sitk.CannyEdgeDetection(sitk.Cast(fg_img, sitk.sitkFloat32))
+        # merge labels that are in contact in the edge image
+        connFilter = sitk.ConnectedComponentImageFilter()
+        connFilter.SetFullyConnected(True)
+        connected_img = connFilter.Execute(edge_img > 0)
+        connected_img = sitk.RelabelComponent(connected_img, minimumObjectSize=100)
+        return connected_img
+
+    def detectPlates(connected_img):
+        # only keep connected components that have two sides larger than one third of the smallest
+        # dimension of the image and the third that is 4 times smaller
+        stats_allObjs = sitk.LabelShapeStatisticsImageFilter()
+        stats_allObjs.ComputeOrientedBoundingBoxOn()
+        stats_allObjs.Execute(connected_img > 0)
+        crop_img = sitk.Crop(connected_img, stats_allObjs.GetBoundingBox(1))
+
+        filtered_img = sitk.Image(crop_img.GetSize(), sitk.sitkUInt8)
+        filtered_img.CopyInformation(crop_img)
+
+        stats = sitk.LabelShapeStatisticsImageFilter()
+        stats.ComputeOrientedBoundingBoxOn()
+        stats.Execute(crop_img)
+        for label in stats.GetLabels():
+            obb_size = np.array(stats.GetOrientedBoundingBoxSize(label))
+            if np.product(obb_size) == 0:
+                continue
+
+            if isPlate_size(stats.GetOrientedBoundingBoxSize(label)) and isPlate_loc(
+                stats.GetCentroid(label),
+                stats_allObjs.GetCentroid(1),
+                stats_allObjs.GetOrientedBoundingBoxSize(1),
+            ):
+                filtered_img = sitk.Or(filtered_img, crop_img == label)
+
+        # close holes in the resulting image
+        closed_img = sitk.BinaryMorphologicalClosing(filtered_img, (1, 1, 1), sitk.sitkBall)
+        # paste the closed image on the original image space
+
+        closed_img_orig = sitk.Image(connected_img.GetSize(), sitk.sitkUInt8)
+        closed_img_orig.CopyInformation(connected_img)
+        closed_img_orig = sitk.Paste(
+            closed_img_orig,
+            closed_img,
+            closed_img.GetSize(),
+            [0, 0, 0],
+            closed_img_orig.TransformPhysicalPointToIndex(
+                closed_img.TransformIndexToPhysicalPoint([0, 0, 0])
+            ),
+        )
+
+        return closed_img_orig
+
+    def refinePlates(in_img):
+        """refine the plates by optimizing a threshold down from the max intensity in the image"""
 
     in_img_np = sitk.GetArrayFromImage(in_img)
     voxelCount = in_img_np.shape[0] * in_img_np.shape[1] * in_img_np.shape[2]
@@ -25,40 +153,19 @@ def segment_zFrame(in_img: sitk.Image, img_type="MR", withPlots=False):
     cumHist_y = np.cumsum(hist_y.astype(float)) / voxelCount
     # we postulate that the background contain half of the voxels
     minThreshold_byCount = hist_x[np.where(cumHist_y > 0.90)[0][0]]
-    print("background threshold by count found: %d" % minThreshold_byCount)
 
-    # Mask in_image with minthreshold_byCount
-    fg_img = in_img * sitk.Cast(in_img > minThreshold_byCount, in_img.GetPixelIDValue())
-    # do edge detection on the fg_img
-    edge_img = sitk.CannyEdgeDetection(sitk.Cast(fg_img, sitk.sitkFloat32))
-    # merge labels that are in contact in the edge image
-    connFilter = sitk.ConnectedComponentImageFilter()
-    connFilter.SetFullyConnected(True)
-    connected_img = connFilter.Execute(edge_img > 0)
-    connected_img = sitk.RelabelComponent(connected_img, minimumObjectSize=100)
-    # only keep connected components that have two sides larger than one third of the smallest
-    # dimension of the image and the third that is 4 times smaller
-    filtered_img = sitk.Image(in_img.GetSize(), sitk.sitkUInt8)
-    filtered_img.CopyInformation(in_img)
+    print(f"background threshold by count found: {minThreshold_byCount}")
 
-    stats = sitk.LabelShapeStatisticsImageFilter()
-    stats.ComputeOrientedBoundingBoxOn()
-    stats.Execute(connected_img)
-    for label in stats.GetLabels():
-        obb_size = np.array(stats.GetOrientedBoundingBoxSize(label))
-        if np.product(obb_size) == 0:
-            continue
-        # compute the ratio between the smallest and the largest side of obb
-        obb_ratio = max(obb_size) / min(obb_size)
-        image_occupy = np.sum(obb_size >= 0.5 * min(in_img.GetSize()))
-        if (image_occupy >= 1) and (obb_ratio > 4) and (np.sum(obb_size > 100) >= 2):
-            print(f"Label {label} is a plate.")
-            print(f"\tSize: {obb_size}")
-            print(f"\tRatio: {obb_ratio}")
-            filtered_img = sitk.Or(filtered_img, connected_img == label)
-    # close holes in the resulting image
-    closed_img = sitk.BinaryMorphologicalClosing(filtered_img, (1, 1, 1), sitk.sitkBall)
-    return closed_img
+    if img_type == "CT":
+        plates_img = threshold_plates_CT(in_img, minThreshold_byCount, hist_y, hist_x, withPlots)
+    elif img_type == "MR":
+        plates_img = threshold_plates_MR(in_img, minThreshold_byCount, withPlots)
+    else:
+        raise ValueError("Unknown image type")
+
+    # paste the plate images on the original image space
+
+    return detectPlates(plates_img)
 
 
 def segment_zFrame_filesystem(in_file, out_file, img_type):
